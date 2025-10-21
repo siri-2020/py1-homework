188f524179a9f78b9911a9002dc3d7f7c5f13667
siri-2020 <tanyakarn.siri2020@gmail.com>
2025-10-21 14:10:42 +0700

    eyyyy

--- day-2.py
+++ day-2.py
@@ -1,6 +1,26 @@
+#print function output the reult in the terminal area
+#function_name(input_1,input_2,input_3)
+# > argument = input f function /we can have multiple arguments separated by commas
 print('Mamamia.')
 print('Let\'s go!')
 print('Woohoo!')
+print(1, 2, 6, 'Hello')
+# sep='-'
+print(1, 2, 6, 'Hello', sep='-')
+# sep='-t-'
+print(1, 2, 6, 'Hello', sep='-t-')
+
+#Date type: String, Integer, Float, Boolean
+
+# [1] Integer: number with no decimal point (Natural numbers)
+3, 5, -3, 201
+# [2] Float: numbers with decimal point
+3.14, -0.001, 2.0
+# [3] String: sequence of characters enclosed in single or double quotes
+'Hello', "The End"
+"5.00"
+# [4] Boolean (Truth Value): True or False values
+True, False
 
 #comment1
 #comment2
