7b2656729c0c826abd42e36186d62f77abdb5247
siri-2020 <tanyakarn.siri2020@gmail.com>
2025-10-23 13:46:32 +0700

    C to F

---
+++ celcius_to_fahrenheit.py
@@ -0,0 +1,7 @@
+celcius = int(input('Input Celsius temperature: '))
+fahrenheit = (Celcius * 1.8) + 32
+print(fahrenheit)
+
+fahrenheit = int(input('Input Fahrenheit temperature: '))
+celsius = (fahrenheit - 32) / 1.8
+print(celsius)
