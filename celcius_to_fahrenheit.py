9021e504da265073517585b46de0a6c18964ec2b
siri-2020 <tanyakarn.siri2020@gmail.com>
2025-10-23 14:11:37 +0700

    update

--- celcius_to_fahrenheit.py
+++ celcius_to_fahrenheit.py
@@ -1,7 +1,10 @@
-celcius = float(input('Input Celsius temperature: '))
+celcius = float(input('Enter Celsius temperature: '))
 fahrenheit = (celcius * 1.8) + 32
-print(fahrenheit)
+print('→ Fahrenheit temperature is', fahrenheit)
+kelvin = (celcius + 273.15)
+print('→ Kelvin temperature is', kelvin)
 
-fahrenheit = float(input('Input Fahrenheit temperature: '))
+# reverse case
+fahrenheit = float(input('Enter Fahrenheit temperature: '))
 celsius = (fahrenheit - 32) / 1.8
-print(celsius)
+print('→ Celcius temperature is', celsius)
